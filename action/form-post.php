ec01d7a4d79344c3633739cd4c6f4d646d7ce95b
ashwani agrawal <ashwani1791@gmail.com>
2022-06-08 19:12:31 +0530

    form submittion adde

---
+++ action/form-post.php
@@ -0,0 +1,10 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+if (!is_writable($root.'data')) {
+    file_put_contents($root.'/data/form.' . time() . ".json", json_encode($_POST));
+    echo "Form Submitted Success";
+} else {
+    echo "Form Not Submitted";
+}
+
+exit;
