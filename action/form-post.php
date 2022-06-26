7eb302f1a6f264cb11b02e84c4ede67e3c6c8cf0
ashwani agrawal <ashwani1791@gmail.com>
2022-06-26 12:36:36 +0530

    file input added and assests added offline

--- action/form-post.php
+++ action/form-post.php
@@ -1,7 +1,7 @@
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
-if (!is_writable($root.'data')) {
-    file_put_contents($root.'/data/form.' . time() . ".json", json_encode($_POST));
+if (!is_writable($root . 'data')) {
+    file_put_contents($root . '/data/form.' . time() . ".json", json_encode([$_POST, $_FILES]));
     echo "Form Submitted Success";
 } else {
     echo "Form Not Submitted";
